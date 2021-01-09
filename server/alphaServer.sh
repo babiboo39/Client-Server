5810693da7d5f4d33711b70b5743078f53ef096d
Ruli Simanungkalit <ruli.kalit39@protonmail.com>
2021-01-09 09:33:14 +0700

    reduce sleep time to 0.3s

--- server/alphaServer.sh
+++ server/alphaServer.sh
@@ -2,7 +2,9 @@
 
 while true
 do
-    (echo $(if [ -e /root/nodeABC.log ]; then grep -o -i "failed password\|accepted password" /root/nodeABC.log | wc -l; else echo "0"; fi; echo " ssh log-in attempts were made at nodeABC")
-    echo $(if [ -e /root/nodeABC.log ]; then grep -o -i "failed password\|accepted password" /root/nodeXYZ.log | wc -l; else echo "0"; fi; echo " ssh log-in attempts were made at nodeXYZ")) > /home/monitor.log
-    sleep 1
+    (echo $(echo "Metrics for ssh log-in attempt(s)")
+    echo $(echo "\n")
+    echo $(echo "nodeABC has "; if [ -e /root/nodeABC.log ]; then grep -o -i "failed password\|accepted password" /root/nodeABC.log | wc -l; else echo "0"; fi; echo " attempt(s)")
+    echo $(echo "nodeXYZ has "; if [ -e /root/nodeABC.log ]; then grep -o -i "failed password\|accepted password" /root/nodeXYZ.log | wc -l; else echo "0"; fi; echo " attempt(s)")) > /home/monitor.log
+    sleep 0.3
 done
