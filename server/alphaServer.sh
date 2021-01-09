550c64018fcaff73ca643df5f4f5ce51d5ae568f
Ruli Simanungkalit <ruli.kalit39@protonmail.com>
2021-01-09 08:45:20 +0700

    modify serached pattern because of pam_systemd issue

--- server/alphaServer.sh
+++ server/alphaServer.sh
@@ -2,6 +2,6 @@
 
 while true
 do
-    (echo $(echo "nodeABC has "; if [ -e /root/nodeABC.log ]; then grep -o -i "failed\|accepted" /root/nodeABC.log | wc -l; else echo "0"; fi; echo " attempt(s)")) > /home/monitor.log
+    (echo $(echo "nodeABC has "; if [ -e /root/nodeABC.log ]; then grep -o -i "failed password\|accepted password" /root/nodeABC.log | wc -l; else echo "0"; fi; echo " attempt(s)")) > /home/monitor.log
     sleep 1
 done
